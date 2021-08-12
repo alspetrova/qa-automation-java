37182673ddfe969f8852519362a1b39f5d7b9c6e
alspetrova <aspetrova90@gmail.com>
2021-08-12 15:36:53 +0300

    Add pom.xml fix 2

--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -9,9 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
