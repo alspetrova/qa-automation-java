a4d02bee2b7b8fc78cf039e2ba256e6637b34835
alspetrova <aspetrova90@gmail.com>
2021-08-15 15:19:14 +0300

    Add 4 classes

---
+++ src/main/java/com/tinkoff/edu/app/LoanCalcRepository.java
@@ -0,0 +1,15 @@
+package com.tinkoff.edu.app;
+
+public class LoanCalcRepository {
+    private static int requestId;
+
+    /**
+     * TODO persist request
+     * @return Request Id
+     */
+
+    public static int save() {
+        int localVar = ++requestId;
+        return localVar;
+    }
+}
---
+++ src/main/java/com/tinkoff/edu/app/LoanCalcService.java
@@ -0,0 +1,13 @@
+package com.tinkoff.edu.app;
+
+public class LoanCalcService {
+    /**
+     * TODO Loan calculation
+     */
+    public static int createRequest() {
+        int localVar;
+        return LoanCalcRepository.save();
+
+
+    }
+}
