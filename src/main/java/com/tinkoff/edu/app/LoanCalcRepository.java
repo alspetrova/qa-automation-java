c26d5c7829b68753ca38c6ae495c6c725f81e39a
alspetrova <aspetrova90@gmail.com>
2021-08-15 15:19:14 +0300

    Fix 4 classes

---
+++ src/main/java/com/tinkoff/edu/app/LoanCalcRepository.java
@@ -0,0 +1,13 @@
+package com.tinkoff.edu.app;
+
+public class LoanCalcRepository {
+    private static int requestId;
+
+    /**
+     * TODO persist request
+     * @return Request Id
+     */
+    public static int save() {
+        return ++requestId;
+    }
+}
