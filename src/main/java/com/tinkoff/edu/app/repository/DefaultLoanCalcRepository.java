7fbdbc2089a61eda240ba6adb3177a512c29d56c
alspetrova <aspetrova90@gmail.com>
2021-08-30 12:05:58 +0300

    Добавила 1) условия для получения Approved и Declined 2) тесты

--- src/main/java/com/tinkoff/edu/app/repository/DefaultLoanCalcRepository.java
+++ src/main/java/com/tinkoff/edu/app/repository/DefaultLoanCalcRepository.java
@@ -13,7 +13,37 @@ public class DefaultLoanCalcRepository implements LoanCalcRepository {
      */
     @Override
     public LoanResponse save(LoanRequest request) {
-        return new LoanResponse(ResponseType.APPROVED, ++requestId, request);
+        if  (request.getMonths() <=0 ||request.getAmount() <=0 || request==null)
+            return new LoanResponse(ResponseType.DECLINED, -1, request);
+        ResponseType responseType = calculateResponseType(request);
+        return new LoanResponse(responseType, ++requestId, request);
+    }
+
+    public ResponseType calculateResponseType(LoanRequest request){
+        if (request.getType().equals(LoanType.PERSON) & request.getMonths() <= 12 & request.getAmount() <= 10_000) {
+            return ResponseType.APPROVED;
+        }
+
+        else if (request.getType().equals(LoanType.PERSON) & request.getMonths() > 12 & request.getAmount() > 10_000) {
+            return ResponseType.DECLINED;
+        }
+
+        else if (request.getType().equals(LoanType.OOO) & request.getAmount() <= 10_000) {
+            return ResponseType.DECLINED;
+        }
+
+        else if (request.getType().equals(LoanType.OOO) & request.getAmount() > 10_000 & request.getMonths() < 12) {
+            return ResponseType.APPROVED;
+        }
+
+        else if (request.getType().equals(LoanType.OOO) & request.getAmount() > 10_000 & request.getMonths() >= 12) {
+            return ResponseType.DECLINED;
+        }
+
+        else if (request.getType().equals(LoanType.IP)) {
+            return ResponseType.DECLINED;
+        }
+        else return ResponseType.DECLINED;
     }
 
 }
--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class AppTest {
     private LoanCalcController calcController;
     private LoanRequest request;
@@ -27,7 +26,7 @@ public class AppTest {
     }
 
     @Test
-    @DisplayName("Проверка, что requestId равен 1 после первого создания запроса")
+    @DisplayName("1: Проверка, что requestId равен 1 после первого создания запроса")
     public void shouldGetOneWhenFirstResponse() {
         //region When
         LoanResponse response = calcController.createRequest(request);
@@ -38,10 +37,104 @@ public class AppTest {
     }
 
     @Test
-    @DisplayName("Проверка, что requestId увеличивается на 1 после каждого создания запроса")
+    @DisplayName("2: Проверка, что requestId увеличивается на 1 после каждого создания запроса")
     public void shouldGetIncrementedIdAnyCall() {
         int firstCall = calcController.createRequest(request).getRequestId();
         int secondCall = calcController.createRequest(request).getRequestId();
         assertEquals(firstCall + 1, secondCall);
+    }
+
+    @Test
+    @DisplayName("3: Ошибка, если запрос null")
+    public void shouldGetErrorWhenApplyNullRequest(){
+        request=null;
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(-1, response.getRequestId());
+   }
+
+    @Test
+    @DisplayName("4: Ошибка, если сумма = 0")
+    public void shouldGetErrorWhenApplyZeroAmountRequest(){
+        request = new LoanRequest(LoanType.PERSON, 10, 0);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(-1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("5: Ошибка, если количество месяцев = 0")
+    public void shouldGetErrorWhenApplyZeroMonthsRequest(){
+        request = new LoanRequest(LoanType.OOO, 0, 12);
+       LoanResponse response = calcController.createRequest(request);
+       assertEquals(-1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("6: Approved for Person, amount<=10_000, months<=12")
+    public void shouldGetApproveWhenPersonLess10000Less12(){
+        request = new LoanRequest(LoanType.PERSON, 10_000, 12);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("7: Declined for Person, amount>10_000, months>12")
+    public void shouldGetDeclineWhenPersonMore10000More12(){
+        request = new LoanRequest(LoanType.PERSON, 10_001, 13);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("8: Declined for OOO, amount<=10_000, any months")
+    public void shouldGetDeclineWhenOOOLess10000(){
+        request = new LoanRequest(LoanType.OOO, 10_000, 1);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("9: Approved for OOO, amount>10_000, months<12")
+    public void shouldGetApproveWhenOOOMore10000Less12(){
+        request = new LoanRequest(LoanType.OOO, 10_001, 11);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("10: Declined for OOO, amount>10_000, months>=12")
+    public void shouldGetDeclineWhenOOOMore10000More12(){
+        request = new LoanRequest(LoanType.OOO, 10_001, 12);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("11: Declined for IP, any amount, any months")
+    public void shouldGetDeclineWhenIP(){
+        request = new LoanRequest(LoanType.IP, 5_000, 5);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @Test
+    @DisplayName("12: Declined for Person, amount>10_000, months<=12")
+    public void shouldGetDeclinePersonMore10000Less12(){
+        request = new LoanRequest(LoanType.PERSON, 11_000, 11);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @DisplayName("13: Declined for Person, amount<10_000, months>=12")
+    public void shouldGetDeclinePersonLess10000More12(){
+        request = new LoanRequest(LoanType.PERSON, 9_000, 13);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
+    }
+
+    @DisplayName("14: Declined for 000, amount<=10_000, months<=12")
+    public void shouldGetDeclineOOOLess10000Less12(){
+        request = new LoanRequest(LoanType.OOO, 9_000, 10);
+        LoanResponse response = calcController.createRequest(request);
+        assertEquals(1, response.getRequestId());
     }
 }
